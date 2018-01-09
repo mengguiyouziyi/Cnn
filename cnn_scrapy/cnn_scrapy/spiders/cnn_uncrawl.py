c925996b9502b73d961f78060ed23419a189ff55
mengguiyouziyi <775618369@qq.com>
2018-01-09 16:57:09 +0800

    v4

--- cnn_scrapy/cnn_scrapy/spiders/cnn_uncrawl.py
+++ cnn_scrapy/cnn_scrapy/spiders/cnn_uncrawl.py
@@ -17,26 +17,26 @@ from cnn_scrapy.items import CnnItem
 
 
 class MeishijieSpider(CrawlSpider):
-    name = 'cnn'
+    name = 'cnn_uncrawl'
     allowed_domains = ['cnn.com']
     custom_settings = {
         'DEFAULT_REQUEST_HEADERS': {
-            'host': "www.cnn.com",
-            'connection': "keep-alive",
-            'cache-control': "no-cache",
-            'upgrade-insecure-requests': "1",
-            'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-            'referer': "http://www.cnn.com/politics",
-            'accept-encoding': "gzip, deflate",
+            'accept': "*/*",
+            'accept-encoding': "gzip, deflate, br",
             'accept-language': "zh-CN,zh;q=0.9,en;q=0.8",
-            # 'cookie': "THEOplayer.textTrackStyle.fontFamily=; THEOplayer.textTrackStyle.fontColor=rgba(255, 255, 255, 1); THEOplayer.textTrackStyle.fontSize=1em; THEOplayer.textTrackStyle.backgroundColor=rgba(0, 0, 0, 0.75); THEOplayer.textTrackStyle.windowColor=rgba(0, 0, 0, 0); THEOplayer.textTrackStyle.edgeStyle=uniform; volume=1; theoplayer-session-id=204e8184-3088-4fc4-80d9-014001888848; metric_enabled=true; metric_downloadBytes=2197784; metric_downloadTime=0.1903000000000361; metric_latency=0.12069899999999052; countryCode=US; geoData=herndon|VA|20170|US|NA; tryThing00=6858; optimizelyEndUserId=oeu1515396174699r0.121411241465331; s_cc=true; s_fid=7799CA5E1940F980-3DF443F2BCF72DE3; __qca=P0-450045770-1515396176267; s_vi=[CS]v1|2D298E28051D217F-400019078000A33C[CE]; __gads=ID=5d791ed372f4e61b:T=1515396179:S=ALNI_MaGgJyZkjkSDSgfAQuKLS2BqfOkzA; gig_hasGmid=ver2; s_ppv=100; optimizelyBuckets=%7B%7D; IPE_S_104983=104983; __vrf=1515410903819BJBuITbGHxjdDrGqXNdSqH82zADtP4QB; optimizelySegments=%7B%22173979470%22%3A%22referral%22%2C%22175262621%22%3A%22gc%22%2C%22175404620%22%3A%22false%22%2C%22175460039%22%3A%22none%22%7D; WSOD%5FcompareToSP500=0; WSOD%5FcompareToCategory=0; __unam=7549672-160d4ab9df2-555ea13f-8; WSOD%5FxrefSymbol=ROKU; _cb_ls=1; bounceClientVisit340v=N4IgNgDiBcIBYBcEQKQGYCCKBMAxHuA7sQHQDGAdheQPYC2BIANCAE4wh00UCmAnuSq06IAL5A; _t_tests={\"[e689addd]\":{\"chosenVariant\":\"B\",\"specificLocation\":[\"*[@id='homepage1-zone-1']/div[2]/div[1]/div[3]/ul[1]/article[5]/div[1]/div[1]/h3[1]/a[1][@href='http://www.cnn.com/videos/world/2018/01/08/china-oil-tanker-freighter-collision-rivers-nr.cnn']\"]},\"[67119b0e]\":{\"chosenVariant\":\"B\",\"specificLocation\":[\"*[@id='homepage1-zone-1']/div[2]/div[1]/div[3]/ul[1]/article[2]/div[1]/div[1]/h3[1]/a[1][@href='http://www.cnn.com/2018/01/07/politics/donald-trump-schedule/index.html']\"]},\"lift_exp\":\"m\"}; ug=5a531c4f0aaa020a3c21445e53045aa3; ugs=1; _cb=Dkpa7gBpzNapq0siR; _cb_svref=http%3A%2F%2Fmoney.cnn.com%2F2018%2F01%2F05%2Ftechnology%2Falexa-headphones-smartwatches%2Findex.html; s_sq=%5B%5BB%5D%5D; hpt=cGFnZV8xNGNvbF9wb2xpdGljc19zZWN0aW9uIGZyb250X3pvbmUtMC0xX1RvcCBuZXdzIGFuZCB0ZXRyaXNfY24tLWV4cGFuZGFibGVfVG9wLVN0b3JpZXM=; hpt2=cGFnZV8xNGNvbF9wb2xpdGljc19zZWN0aW9uIGZyb250X3pvbmUtMC0xX3ByaW9yaXR5KzJfY24tLWV4cGFuZGFibGVfbGlzdDpoZWFkbGluZXMraW1hZ2Vz; _uetsid=_uet7d7e2789; session_depth=www.cnn.com%3D1%7C505661222%3D1; _chartbeat2=.1515410937879.1515411725229.1.DyjSoX391EIDJbCtCBYVgcuBZaPor; GED_PLAYLIST_ACTIVITY=W3sidSI6IkpFc2YiLCJ0c2wiOjE1MTU0MTIwODUsIm52IjoxLCJ1cHQiOjE1MTU0MTIwNzcsImx0IjoxNTE1NDEyMDg0fV0.",
+            'origin': "http://edition.cnn.com",
+            'referer': "http://edition.cnn.com/search/?size=10&q=technology&from=30&page=4",
+            'user-agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36",
+            'cache-control': "no-cache",
+            'postman-token': "4d85af38-befa-3602-b49e-32bf9f803900"
         },
         'DOWNLOAD_DELAY': 1
     }
     start_urls = ['http://edition.cnn.com/search/?size=10&q=technology', 'http://www.cnn.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=('technology|politics|us', '/search/\?size=\d+\&q=(technology|politics|us|china)\&from=\d+\&page=\d+'))),
+        Rule(LinkExtractor(allow=(
+        'technology|politics|us', '/search/\?size=\d+\&q=(technology|politics|us|china)\&from=\d+\&page=\d+'))),
         Rule(LinkExtractor(
             allow=('(technology|politics|us).*index\.html$'), deny=('videos', 'fortune', 'interactive')),
             callback='parse_item', follow=True),
